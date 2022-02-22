9be447ac0dd76829d2dd8f2fd85d558ccef6d897
skyangelsUA <dkskyangels@gmail.com>
2022-02-22 21:50:54 +0200

    JS1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+
+
+
+const imagesAdd = document.querySelector(".gallery");
+
+
+let makeString = '';
+
+galleryItems.forEach(({ preview, original, description }) => {
+    makeString += `<a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt='${description}'
+    /></a>`
+});
+
+
+imagesAdd.insertAdjacentHTML('beforeend',makeString);
+imagesAdd.addEventListener('click', getClick);
+
+function getClick(event) {
+    event.preventDefault()
+
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+
+    const instance = basicLightbox.create(`
+        <img src="${event.target.dataset.source}">
+    `)
+
+    instance.show();
+}
 
 console.log(galleryItems);
