2347ee8190828235b893e4750c95ed0c4aa0f547
skyangelsUA <dkskyangels@gmail.com>
2022-09-04 02:34:53 +0300

    Комит ебала эта форма, эту форму дом  труба шатал!!!!!!!!!!!

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,24 +1,28 @@
 import throttle from 'lodash.throttle';
 
-const checkForm =document.querySelector('.feedback-form');
-const formData = {
-    email: '',
-    message: '',
-};
+const checkForm = document.querySelector('.feedback-form');
+const email = document.querySelector('[name="email"]');
+const message = document.querySelector('[name="message"]');
 
 
 const LOCAL_STORAGE_KEY = "feedback-form-state";
 
 checkForm.addEventListener('submit', onFormSubmit);
 checkForm.addEventListener('input', throttle(onInput, 500));
-onStorageData();
 
-function onInput(event) {
-    formData[event.target.name] = event.target.value;
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
-    
+
+function onInput() {
+
+    const formData = {
+    email: email.value,
+    message: message.value,
+  };
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
+  
+  console.log(formData)
+  
 };
-console.log(onInput)
+
 
 
 
@@ -26,24 +30,24 @@ function onFormSubmit(event) {
    
     event.preventDefault();
     event.currentTarget.reset();
+    console.log(localStorage.getItem(LOCAL_STORAGE_KEY));
     localStorage.removeItem(LOCAL_STORAGE_KEY);
     
-    console.log(formData)
+    
 };
 
 
-function onStorageData() {
-    const savedData = localStorage.getItem(LOCAL_STORAGE_KEY);
-    const savedDataParse = JSON.parse(savedData);
-  if (savedDataParse) {
-    returnDataInput(savedDataParse);
+
+
+localStorageSave();
+
+function localStorageSave() {
+
+  let storageSave = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+
+  if (storageSave) {
+    email.value = storageSave.email;
+    message.value = storageSave.message;
   }
+
 }
-
-
-  function returnDataInput(data) {
-    formData.email = data.email;
-    formData.message = data.message;
-    checkForm.elements.email.value = formData.email;
-    checkForm.elements.message.value = formData.message;
-  }
